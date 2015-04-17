692283c8de85257ab32c151c8c79f8f123315f3f
Valery Ivanov <valeryi@proekspert.ee>
2015-04-17 23:01:43 +0300

    Location in create view

--- app/src/main/java/com/walts/ideas/activities/CreateIdeaActivity.java
+++ app/src/main/java/com/walts/ideas/activities/CreateIdeaActivity.java
@@ -1,14 +1,20 @@
 package com.walts.ideas.activities;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.walts.ideas.Dialogs;
+import com.walts.ideas.LocationHelper;
 import com.walts.ideas.R;
 import com.walts.ideas.db.Idea;
 import com.walts.ideas.db.IdeasDbHelper;
@@ -19,10 +25,17 @@ public class CreateIdeaActivity extends ActionBarActivity {
 
     private IdeasDbHelper dbHelper = IdeasDbHelper.getInstance(this);
 
+    private Location location = null;
+    private String address = null;
+
+    private LocationHelper locationHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_idea);
+
+        locationHelper = new LocationHelper(this);
     }
 
     @Override
@@ -53,6 +66,15 @@ public class CreateIdeaActivity extends ActionBarActivity {
         } else {
             Idea idea = new Idea(title, desc);
 
+            if (location != null) {
+                idea.latitude = location.getLatitude();
+                idea.longitude = location.getLongitude();
+            }
+
+            if (address != null) {
+                idea.address = address;
+            }
+
             long id = dbHelper.insertIdea(idea);
             if (id == -1) {
                 //ERROR
@@ -79,5 +101,78 @@ public class CreateIdeaActivity extends ActionBarActivity {
         finish();
 
         Toast.makeText(this, R.string.idea_created, Toast.LENGTH_SHORT).show();
+    }
+
+    public void addOrRemoveLocation(final View view) {
+        final Button button = (Button) findViewById(R.id.addOrRemoveLocation_button);
+        if (location == null) {
+
+            button.setEnabled(false);
+
+            final ProgressBar progressBar = (android.widget.ProgressBar) findViewById(R.id.progressBar);
+            progressBar.setVisibility(View.VISIBLE);
+
+            LocationHelper.LocationResult locationResult = new LocationHelper.LocationResult() {
+                @Override
+                public void gotLocation(final Location location){
+                    CreateIdeaActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (location != null) {
+                                CreateIdeaActivity.this.location = location;
+                                String address = locationHelper.getAddress(location.getLatitude(), location.getLongitude());
+
+                                progressBar.setVisibility(View.GONE);
+                                findViewById(R.id.progressBar_container).setVisibility(View.GONE);
+
+                                TextView latitudeTextView = (TextView) findViewById(R.id.latitude);
+                                latitudeTextView.setText(String.valueOf(location.getLatitude()));
+
+                                TextView longitudeTextView = (TextView) findViewById(R.id.longitude);
+                                longitudeTextView.setText(String.valueOf(location.getLongitude()));
+
+                                findViewById(R.id.location_container).setVisibility(View.VISIBLE);
+
+                                button.setText(getString(R.string.remove_location));
+                                button.setEnabled(true);
+
+                                if (address != null) {
+                                    TextView addressTextView = (TextView) findViewById(R.id.address);
+                                    addressTextView.setText(address);
+                                } else {
+                                    Toast.makeText(CreateIdeaActivity.this, R.string.address_fetching_failed, Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Dialogs.showAlertMessage(CreateIdeaActivity.this, new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), getString(R.string.location_null));
+                            }
+                        }
+                    });
+                }
+            };
+            boolean requestSuccessful = locationHelper.requestLocation(locationResult);
+            if (!requestSuccessful) {
+                progressBar.setVisibility(View.GONE);
+                button.setEnabled(true);
+            }
+        } else {
+            location = null;
+            address = null;
+
+            TextView latitudeTextView = (TextView) findViewById(R.id.latitude);
+            latitudeTextView.setText("");
+
+            TextView longitudeTextView = (TextView) findViewById(R.id.longitude);
+            longitudeTextView.setText("");
+
+            TextView addressTextView = (TextView) findViewById(R.id.address);
+            addressTextView.setText("");
+
+            button.setText(R.string.add_current_location);
+
+            findViewById(R.id.location_container).setVisibility(View.GONE);
+
+            findViewById(R.id.progressBar_container).setVisibility(View.VISIBLE);
+        }
+
     }
 }
--- app/src/main/java/com/walts/ideas/activities/ListIdeasActivity.java
+++ app/src/main/java/com/walts/ideas/activities/ListIdeasActivity.java
@@ -3,9 +3,7 @@ package com.walts.ideas.activities;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Location;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.text.InputType;
 import android.view.Menu;
@@ -16,10 +14,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.walts.ideas.Dialogs;
 import com.walts.ideas.IdeasAdapter;
 import com.walts.ideas.LocationHelper;
 import com.walts.ideas.R;
@@ -39,14 +34,11 @@ public class ListIdeasActivity extends ActionBarActivity {
     private List<Idea> ideas = new ArrayList<>();
     private IdeasAdapter arrayAdapter;
     private ListView listView;
-    private LocationHelper locationHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_ideas);
-
-        locationHelper = new LocationHelper(this);
 
         populateIdeas();
         populateListView();
@@ -149,34 +141,6 @@ public class ListIdeasActivity extends ActionBarActivity {
         Intent intent = new Intent(this, CreateIdeaActivity.class);
         startActivity(intent);
         finish();
-    }
-
-    public void getLocation(final View view) {
-        view.setEnabled(false);
-        final ProgressBar progressBar = (android.widget.ProgressBar) findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.VISIBLE);
-        LocationHelper.LocationResult locationResult = new LocationHelper.LocationResult() {
-            @Override
-            public void gotLocation(final Location location){
-                ListIdeasActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (location != null) {
-                            Toast.makeText(ListIdeasActivity.this, "Latitude : " + location.getLatitude() + ", longitude : " + location.getLongitude() + ", location is : " + locationHelper.getAddress(location.getLatitude(), location.getLongitude()), Toast.LENGTH_SHORT).show();
-                        } else {
-                            Dialogs.showAlertMessage(ListIdeasActivity.this, new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), getString(R.string.location_null));
-                        }
-                        progressBar.setVisibility(View.GONE);
-                        view.setEnabled(true);
-                    }
-                });
-            }
-        };
-        boolean requestSuccessful = locationHelper.requestLocation(locationResult);
-        if (!requestSuccessful) {
-            progressBar.setVisibility(View.GONE);
-            view.setEnabled(true);
-        }
     }
 
 }
